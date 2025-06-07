f40cab0356e601004e3d423a91feeb1685ef939a
kippel <kippeld@gmail.com>
2025-06-07 07:32:15 +0200

    zod

--- src/db/seed.tsx
+++ src/db/seed.tsx
@@ -1,22 +1,25 @@
 // drizzle/seed.ts
 import { db } from './db'; // tu instancia de Drizzle
 import { users } from './schema'; // tu esquema
-import { eq } from 'drizzle-orm';
+//import { eq } from 'drizzle-orm';
 import bcrypt from "bcryptjs"
 
 async function main() {
-    const hashedPassword = await bcrypt.hash("qwerty", 12)
-    
-        const data = {
-            name: "kippel_",
-            email: "emai@l.com",
-            password: hashedPassword
-        }
-    
-        //console.log(data)
-        const red = await db.insert(users).values(data);
-    console.log('⚠️ Seed ya fue ejecutado antes.');
+
+  await db.delete(users); 
   
+  const hashedPassword = await bcrypt.hash("qwerty", 12)
+
+  const data = {
+    name: "kippel",
+    email: "emai@l.com",
+    password: hashedPassword,
+  }
+
+  //console.log(data)
+  await db.insert(users).values(data);
+  console.log('⚠️ Seed ya fue ejecutado antes.');
+
 }
 
 main()
