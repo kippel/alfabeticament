785f3c02d838ff56b32c83bc32326f21ff3bdeb6
kippel <kippeld@gmail.com>
2025-06-16 16:22:49 +0200

    lang

--- src/app/(abc)/abc/languages/page.tsx
+++ src/app/(abc)/abc/languages/page.tsx
@@ -1,8 +1,26 @@
+import { getCourses } from "@/db/queries";
+import { List } from "./list";
 
 
-function LanguagesPage(){
+const LanguagesPage = async () => {
+//export default async function LangPage(){
+
+    //const { data: session, status } = useSession();
+
+    //console.log(session, status);
+    const courses = await getCourses();
+    //await getUserProgress();
     return (
-        <div className="h-full max-w-[912px] px-3 mx-auto"><h1 className="text-2xl font-bold text-gray-200">Languages</h1></div>
+        <div className="h-full max-w-[912px] px-3 mx-auto">
+            <h1 className="text-2xl font-bold text-gray-200">Languages</h1>
+         
+        <List
+          courses={courses}
+          activeCourseId={1}
+        />
+        
+        
+        </div>
     )
 }
 
