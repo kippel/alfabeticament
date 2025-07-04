125071c1cbedd913b051c6280f3c24653e4e632b
kippel <kippeld@gmail.com>
2025-07-04 12:05:44 +0200

    seed

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashedPassword = await bcrypt.hash('admin123', 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name: 'Admin4',
+      password: hashedPassword,
+    },
+  });
+
+  console.log('User created or already exists:', user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
